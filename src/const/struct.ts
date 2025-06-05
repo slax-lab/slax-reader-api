0c382ea443077d1b660ee5846746373606dd7257
daguang <daguang830@gmail.com>
2025-06-05 14:26:10 +0800

    🐛 changing twitter resolution service provider

--- src/const/struct.ts
+++ src/const/struct.ts
@@ -1,72 +1,182 @@
-interface User {
-  created_at: string // 用户创建账户的日期和时间
-  default_profile_image: boolean // 是否使用默认的个人资料图片
-  description: string // 用户个人简介
-  fast_followers_count: number // 快速跟随者的数量（可能特定于某种功能）
-  favourites_count: number // 用户点赞的推文总数
-  followers_count: number // 关注用户的人数
-  friends_count: number // 用户关注的人数
-  has_custom_timelines: boolean // 是否启用了自定义时间线
-  is_translator: boolean // 用户是否是翻译者
-  listed_count: number // 用户被列入的列表数
-  location: string // 用户的位置信息
-  media_count: number // 用户发布的媒体数量
-  name: string // 用户的显示名称
-  normal_followers_count: number // 普通关注者的数量
-  possibly_sensitive: boolean // 用户账户是否可能包含敏感内容
-  profile_banner_url: string // 用户的个人资料横幅图片URL
-  profile_image_url_https: string // 用户的个人资料图片URL
-  screen_name: string // 用户的用户名（@username）
-  statuses_count: number // 用户发布的推文数量
-  translator_type: string // 翻译者类型（如果适用）
-  url: string // 用户个人资料中的URL
-  verified: boolean // 用户是否经过验证
-  withheld_in_countries: string[] // 被限制的国家列表
-  id_str: string // 用户的ID（字符串格式）
+interface TwitterAPIResponse {
+  tweets: TweetInfo[]
+  status: string
+  msg: string
+  code: number
 }
 
-interface Url {
-  url: string // 短URL
-  expanded_url: string // 展开后的完整URL
-  display_url: string // 显示的URL（可能经过缩短）
+interface TweetInfo {
+  type: string
+  id: string
+  url: string
+  twitterUrl: string
+  text: string
+  source: string
+  retweetCount: number
+  replyCount: number
+  likeCount: number
+  quoteCount: number
+  viewCount: number
+  createdAt: string
+  lang: string
+  bookmarkCount: number
+  isReply: boolean
+  inReplyToId: string
+  conversationId: string
+  inReplyToUserId: string
+  inReplyToUsername: string
+  isPinned: boolean
+  author: Author
+  extendedEntities: ExtendedEntities
+  card: Record<string, any>
+  place: Record<string, any>
+  entities: TweetEntities
+  isRetweet: boolean
+  isQuote: boolean
+  isConversationControlled: boolean
+  quoted_tweet: TweetInfo | null
+  retweeted_tweet: TweetInfo | null
 }
 
-interface Media {
-  media_url: string // 媒体文件的URL
-  video_url?: string // （可选）视频文件的URL
-  type: string // 媒体类型（例如 "photo", "video"）
+interface Author {
+  type: string
+  userName: string
+  url: string
+  twitterUrl: string
+  id: string
+  name: string
+  isVerified: boolean
+  isBlueVerified: boolean
+  profilePicture: string
+  coverPicture: string
+  description: string
+  location: string
+  followers: number
+  following: number
+  status: string
+  canDm: boolean
+  canMediaTag: boolean
+  createdAt: string
+  entities: AuthorEntities
+  fastFollowersCount: number
+  favouritesCount: number
+  hasCustomTimelines: boolean
+  isTranslator: boolean
+  mediaCount: number
+  statusesCount: number
+  withheldInCountries: any[]
+  affiliatesHighlightedLabel: Record<string, any>
+  possiblySensitive: boolean
+  pinnedTweetIds: string[]
+  profile_bio: ProfileBio
+  isAutomated: boolean
+  automatedBy: any
+}
+
+interface AuthorEntities {
+  description: {
+    urls: UrlEntity[]
+    user_mentions: UserMention[]
+  }
+}
+
+interface ProfileBio {
+  description: string
+  entities: {
+    description: {
+      urls: UrlEntity[]
+      user_mentions: UserMention[]
+    }
+  }
+  withheld_in_countries: any[]
+}
+
+interface UrlEntity {
+  display_url: string
+  expanded_url: string
+  indices: number[]
+  url: string
 }
 
 interface UserMention {
-  id_str: string // 被提及用户的ID（字符串格式）
-  name: string // 被提及用户的显示名称
-  screen_name: string // 被提及用户的用户名
-  profile?: string // （可选）被提及用户的个人资料链接
+  id_str: string
+  indices: number[]
+  name: string
+  screen_name: string
 }
 
-interface TweetItem {
-  user: User // 发推文的用户信息
-  id: string // 推文的ID
-  conversation_id: string // 推文所属会话的ID
-  full_text: string // 推文的完整文本
-  reply_count: number // 回复的数量
-  retweet_count: number // 转推的数量
-  favorite_count: number // 点赞的数量
-  hashtags: string[] // 推文中的话题标签
-  symbols: string[] // 推文中的金融符号
-  user_mentions: UserMention[] // 推文中提及的用户列表
-  urls: Url[] // 推文中的URL列表
-  media: Media[] // 推文中包含的媒体列表
-  url: string // 推文的URL
-  created_at: string // 推文发布的日期和时间
-  '#sort_index': string // 推文的排序索引
-  view_count: number // 推文的查看次数
-  quote_count: number // 推文的引用次数
-  is_quote_tweet: boolean // 是否是引用推文
-  is_retweet: boolean // 是否是转推
-  is_pinned: boolean // 是否被置顶
-  is_truncated: boolean // 推文内容是否被截断
-  startUrl: string // 推文的起始URL
-  error?: string // （可选）如果有错误，记录错误信息
-  replying_to_tweet?: string // （可选）回复的推文的URL
+interface TweetEntities {
+  urls: UrlEntity[]
 }
+
+interface ExtendedEntities {
+  media: MediaEntity[]
+}
+
+interface MediaEntity {
+  allow_download_status: {
+    allow_download: boolean
+  }
+  display_url: string
+  expanded_url: string
+  ext_media_availability: {
+    status: string
+  }
+  features: MediaFeatures
+  id_str: string
+  indices: number[]
+  media_key: string
+  media_results: MediaResults
+  media_url_https: string
+  original_info: OriginalInfo
+  sizes: MediaSizes
+  type: string
+  url: string
+}
+
+interface MediaFeatures {
+  large: MediaFaceRecognition
+  orig: MediaFaceRecognition
+}
+
+interface MediaFaceRecognition {
+  faces?: Face[]
+}
+
+interface Face {
+  h: number
+  w: number
+  x: number
+  y: number
+}
+
+interface MediaResults {
+  id: string
+  result: {
+    __typename: string
+    id: string
+    media_key: string
+  }
+}
+
+interface OriginalInfo {
+  focus_rects: FocusRect[]
+  height: number
+  width: number
+}
+
+interface FocusRect {
+  h: number
+  w: number
+  x: number
+  y: number
+}
+
+interface MediaSizes {
+  large: {
+    h: number
+    w: number
+  }
+}
+
+export type { TwitterAPIResponse, TweetInfo, Author, MediaEntity, ExtendedEntities, TweetEntities, AuthorEntities, UrlEntity, UserMention }
